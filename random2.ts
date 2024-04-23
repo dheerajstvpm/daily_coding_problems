fdb9eb541dbb5b55446ac89a5db572a946b7e286
Dheeraj S <dheerajstvpm@gmail.com>
2024-04-23 08:46:49 +0530

    random 2

---
+++ random2.ts
@@ -0,0 +1,12 @@
+const n = 5;
+for (let i = 0; i < n; i++) {
+    let str = '';
+    for (let j = 0; j < n; j++) {
+        if (i === j) {
+            str += `${j + 1} `;
+        } else {
+            str += `  `;
+        }
+    }
+    console.log(str);
+}
