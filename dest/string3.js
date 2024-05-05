2088d5874a26c3185683a9d73589f0320c78f4db
Dheeraj S <dheerajstvpm@gmail.com>
2024-05-05 22:43:16 +0530

    string3

---
+++ dest/string3.js
@@ -0,0 +1,19 @@
+"use strict";
+/**
+ * Convert M1I1S2I1S2I1P2I1 to MISSISSIPPI
+ */
+function converter(stringToConvert) {
+    let convertedString = '';
+    let subString = '';
+    for (let i = 0; i < stringToConvert.length; ++i) {
+        if (/\d/.test(stringToConvert[i])) {
+            convertedString += subString.repeat(Number(stringToConvert[i]));
+            subString = '';
+        }
+        else {
+            subString += stringToConvert[i];
+        }
+    }
+    return convertedString;
+}
+console.log(converter(`M1I1S2I1S2I1P2I1`));
---
+++ promise.ts
@@ -0,0 +1,34 @@
+// Define a function that returns a Promise
+function myAsyncFunction() {
+    return new Promise((res, err) => {
+        // Simulating an asynchronous operation (e.g., fetching data)
+        setTimeout(() => {
+            const randomNumber = Math.random();
+            // Resolve the promise if the random number is greater than 0.5
+            if (randomNumber > 0.5) {
+                res(randomNumber);
+            } else {
+                // Reject the promise if the random number is less than or equal to 0.5
+                err(new Error("Random number is too low"));
+            }
+        }, 1000); // Simulating a delay of 1 second
+    });
+}
+
+// Using the Promise
+const run = async () => {
+    try {
+        const result = await myAsyncFunction();
+        console.log(result);
+    } catch (error:any) {
+        console.log(error.message);
+    }
+}
+run();
+myAsyncFunction()
+    .then((result) => {
+        console.log("Success:", result);
+    })
+    .catch((error) => {
+        console.error("Error:", error.message);
+    });
